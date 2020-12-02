32e8dc743c3cecb8bffdd9443405d5cda4264d16
Thibault Laude <laudethibault_pro@outlook.fr>
2020-12-02 15:58:52 +0100

    correction use

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Service\GestionContact;
+use App\Entity\Contact;
+use App\Form\ContactType;
 
+/**
+ * @Route("/contact", name="contact")
+ */
 class ContactController extends AbstractController {
 
-  /**
-   * @Route("/contact", name="contact")
-   */
   public function index(): Response {
     return $this->render('contact/index.html.twig', [
                 'controller_name' => 'ContactController',
@@ -36,9 +40,9 @@ class ContactController extends AbstractController {
       $gestionContact->envoiMailContact($contact);
 
 
-      return $this->redirectToRoute("tutoriel");
+      return $this->redirectToRoute("contact");
     }
-    return $this->render('tutoriel/contact.html.twig', [
+    return $this->render('contact/contact.html.twig', [
                 'form' => $form->createView(),
     ]);
   }
